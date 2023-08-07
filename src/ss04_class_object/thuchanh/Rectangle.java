9527836c1e10cc9f419809b5476223e0dd0bc6be
Vanloann <vanloan1042001@gmail.com>
2023-08-07 18:52:38 +0700

    Van Loan nop bai thuc hanh bai 4

---
+++ src/ss04_class_object/thuchanh/Rectangle.java
@@ -0,0 +1,43 @@
+package ss04_class_object.thuchanh;
+
+import java.util.Scanner;
+
+public class Rectangle {
+    double width;
+    double height;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double width,double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return  this.width * this.height;
+    }
+
+    public double getPerimeter() {
+        return (this.height + this.width) * 2;
+    }
+
+    public String display() {
+        return "Rectangle{" + "width = " + width + ", height =" + height + "}";
+    }
+
+    public static class Main {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter width: ");
+            double width = scanner.nextDouble();
+            System.out.println("Enter height:");
+            double height = scanner.nextDouble();
+
+            ss04_class_and_object.thuchanh.Rectangle rectangle = new ss04_class_and_object.thuchanh.Rectangle(width, height);
+            System.out.println("Your rectanlge \n" + rectangle.display());
+            System.out.println("Perimeter of the rectangle: " + rectangle.getPerimeter());
+            System.out.println("Area of the rectanlge: " + rectangle.getArea());
+        }
+    }
+}
