0a5fc717b9f6a05a9996bf1497ad3971d872694b
Vanloann <vanloan1042001@gmail.com>
2023-08-18 07:25:04 +0700

    up ss13

--- src/ss13_thuat_toan_tim_kiem/exercise/LongestString.java
+++ src/ss13_thuat_toan_tim_kiem/exercise/LongestString.java
@@ -10,21 +10,35 @@ public class LongestString {
         String string = scanner.nextLine();
 
         LinkedList<Character> max = new LinkedList<>();
-        LinkedList<Character> list = new LinkedList<>();
 
         for (int i = 0; i < string.length(); i++) {
-            if (list.size() > 1 && string.charAt(i) <= list.getLast() && list.contains(string.charAt(i))) {
-                list.clear();
-            }
+            LinkedList<Character> list = new LinkedList<>();
             list.add(string.charAt(i));
+            for (int j = i + 1; j < string.length(); j++) {
+                if (string.charAt(j) > list.getLast()) {
+                    list.add(string.charAt(j));
+                } else {
+                    break;
+                }
+            }
             if (list.size() > max.size()) {
                 max.clear();
                 max.addAll(list);
             }
         }
+//        for (int i = 0; i < string.length(); i++) {
+//            if (list.size() > 1 && string.charAt(i) <= list.getLast() && list.contains(string.charAt(i))) {
+//                list.clear();
+//            }
+//            list.add(string.charAt(i));
+//            if (list.size() > max.size()) {
+//                max.clear();
+//                max.addAll(list);
+//            }
+//        }
+        System.out.println();
         for (Character chaz : max) {
             System.out.print(chaz);
         }
-        System.out.println();
     }
 }
