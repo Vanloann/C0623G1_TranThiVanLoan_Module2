b2f762b994f82853c982fa2270b07bd346c9244d
Vanloann <vanloan1042001@gmail.com>
2023-08-21 16:13:43 +0700

    up ss15

---
+++ src/ss15_exception/exercise/IllegalTriangleException.java
@@ -0,0 +1,11 @@
+package ss15_exception.exercise;
+
+import java.util.Scanner;
+
+public class IllegalTriangleException extends Exception{
+
+    public IllegalTriangleException(String msg) {
+        super(msg);
+    }
+}
+
--- src/ss15_exception/exercise/TriangleExample.java
+++ src/ss15_exception/exercise/TriangleExample.java
@@ -1,22 +1,19 @@
 package ss15_exception.exercise;
 
-import javax.sound.midi.Soundbank;
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class TriangleExample {
-    public void Triangle(int a, int b, int c) throws Exception {
+    public static void triangle(int a, int b, int c) throws Exception {
         if (a < 0 || b < 0 || c < 0) {
-            throw new Exception("Sides of triangle must be positive integer!");
+            throw new IllegalTriangleException("Sides of triangle must be positive integer!");
         } else if (a + b <= c || a + c <= b || b + c <= a) {
-            throw new Exception("Sum of two sides must greater than the other side!");
+            throw new IllegalTriangleException("Sum of two sides must greater than the other side!");
         } else {
 
         }
     }
 
     public static void main(String[] args) {
-        TriangleExample triangleExample = new TriangleExample();
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a: ");
         int a = sc.nextInt();
@@ -25,11 +22,12 @@ public class TriangleExample {
         System.out.println("Enter c: ");
         int c = sc.nextInt();
         try {
-            triangleExample.Triangle(a, b, c);
+            TriangleExample.triangle(a, b, c);
             System.out.println("3 sides of your triangle: " + a + ", " + b + ", " + c);
-        } catch (Exception e) {
+        } catch (IllegalTriangleException e) {
             System.out.println(e.getMessage());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
-
